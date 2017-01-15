69c84b5fd36460490ac58aae13eefd47bdbf4109
checkmyfunk <vitali.potchekin@gmail.com>
2017-01-15 10:40:45 -0500

    Initial Commit

---
+++ Razam/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Razam
+//
+//  Created by Vitali Potchekin on 1/15/17.
+//  Copyright © 2017 Vitali Potchekin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
